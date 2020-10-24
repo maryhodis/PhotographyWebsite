5a56f5ff0fd95cb10c9d6db7ff5a24b8cad8c667
maryhodis <43175966+maryhodis@users.noreply.github.com>
2020-10-24 21:51:33 +0200

    add new page - fashion portfolio

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fields.passwordCheck = document.getElementById('passwordCheck');
     fields.question = document.getElementById('question');
 });
+
 function isNotEmpty(value) {
     if (value == null || typeof value == 'undefined' ) return false;
     return (value.length > 0);
@@ -87,3 +88,10 @@ function sendContact(){
         alert("There was an error.")
     }
 }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
